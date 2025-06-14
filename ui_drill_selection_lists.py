fc56676d344ae09e3e4051175f0b1380b5dbd3e9
davidcjordan <djordan@gmavt.net>
2025-06-14 10:13:43 -0400

    changed ui_drill_selections_lists.py

--- ui_drill_selection_lists.py
+++ ui_drill_selection_lists.py
@@ -1,4 +1,4 @@
-workout_list = [1,3]
+workout_list = [1,2,3]
 
 default_recents_drill_list = [1, 2, 5, 6, 8, 9, 11, 22, 26, 27]
 
@@ -7,7 +7,7 @@ line_drill_dict = {"1-line":[30,31,32,33,34,35,36,38], "2-line":[50,51,52,53,55,
 movement_drill_list = [8,13,22]
 situational_drill_list = [5,6,23]
 
-stroketype_drill_dict = {"ground":[9,1,16,17,18,15,26], "volley":[10,3,25], "net": [11,14,2,27], "lob": [12,4]}
+stroketype_drill_dict = {"ground":[1,2,3,4,5,6,7], "volley":[10,3,25], "net": [11,14,2,27], "lob": [12,4]}
 
 # a1_line_list = [30,31,32,33,34,35,36,38]
 # a2_line_list = [50,51,52,53,54,55,56,57,58,59,60]
